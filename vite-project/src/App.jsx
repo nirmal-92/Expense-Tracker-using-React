76cf1cbe04c4889f303dbf8f3b0e0a2d7a2b3b78
nirmal-92 <nn3134395@gmail.com>
2024-03-11 14:30:34 +0530

    Expense commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,26 +8,26 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>Expense Tracker</div>
+      <div className='income-expense-container'>
+        <div className='income'>
+          <div className='title'>
+            Income
+          </div>
+          <div>
+            5000
+          </div>
+          </div>
+          <div className='block'></div>
+          <div className='expense'>
+            <div className='title'>
+              Expense
+            </div>
+            <div>
+              -300
+            </div>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
